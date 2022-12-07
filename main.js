5b98a0f65308e6e2becce9edb3722cd347e78d7a
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-12-07 10:29:06 -0700

    awd

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Variables
-let usernameCheck = document.getElementById("username");
-let passwordCheck = document.getElementById("password");
 
-// Event Listeners
+// Event Listener
 document.getElementById("login-btn").addEventListener("click", login);
 
 // Function
 function login() {
+    let usernameCheck = document.getElementById("username-in").value;
+    let passwordCheck = document.getElementById("password-in").value;
+
     if (usernameCheck === "admin" && passwordCheck === "1234") {
-        alert("login successful");
+        alert("Login SUCCESSFUL.");
     } else {
-        alert("nonono");
+        alert("Login UNSUCCESSFUL.");
     }
 }
